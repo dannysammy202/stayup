b1cf069c3827471b9a3eff954d51fece9db88882
dannysammy202 <98893120+dannysammy202@users.noreply.github.com>
2026-09-08 00:55:47 +0100

    Complete Getting to Know You friend set at 1000

---
+++ src/data/editorial/getting-to-know-you.friend.supplement.js
@@ -0,0 +1,19 @@
+export default {
+  Chill: [
+    'What is one ordinary thing you would miss quickly if it disappeared from your routine?',
+    'What kind of plan usually sounds good to you at the end of a long week?',
+    'What is something you almost always enjoy more than you expect to?',
+  ],
+  Interesting: [
+    'What is one choice you make differently when nobody else will know about it?',
+    'What is something you became interested in for a reason people would not guess?',
+  ],
+  Deep: [
+    'What is one part of your life where you are trying to trust yourself more?',
+    'What is something you have learnt to stop explaining to people who do not understand it?',
+  ],
+  'No Filter': [
+    'What is one thing you know you make harder for people who care about you?',
+    'What is something you secretly hope people notice without you having to mention it?',
+  ],
+}
