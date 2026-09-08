c440403228bbd96bcb3b92e789d601f17f7812cc
dannysammy202 <98893120+dannysammy202@users.noreply.github.com>
2026-09-08 12:50:54 +0100

    Route Nigeria-first games through cleaned v4 layer

--- src/data/nigeria-first/index.js
+++ src/data/nigeria-first/index.js
@@ -1,6 +1,6 @@
 import { allCategories, conversationCategories, gameCategories } from '../categories.js'
 import { buildNigeriaConversationPrompts, nigeriaConversationCategoryIds } from './conversations-v3.js'
-import { buildNigeriaGamePrompts, nigeriaGameCategoryIds } from './games-v3.js'
+import { buildNigeriaGamePrompts, nigeriaGameCategoryIds } from './games-v4.js'
 
 const conversationIds = new Set(conversationCategories.map(item => item.id))
 const gameIds = new Set(gameCategories.map(item => item.id))
