3707a78331cbbb86e70fbc818ea117239f2a53c1
dannysammy202 <98893120+dannysammy202@users.noreply.github.com>
2026-09-08 15:21:03 +0100

    Route StayUp through conversation-starter interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AppV2 from './AppV2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppV2 />
   </React.StrictMode>,
 )
