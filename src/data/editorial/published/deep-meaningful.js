5353513003745016d186e44c82ab26fc4572702f
dannysammy202 <98893120+dannysammy202@users.noreply.github.com>
2026-09-08 01:12:29 +0100

    Include Deep and Meaningful friend supplement

--- src/data/editorial/published/deep-meaningful.js
+++ src/data/editorial/published/deep-meaningful.js
@@ -1,9 +1,10 @@
 import { mergeGroups, publishBalanced } from '../helpers.js'
 import friend from '../deep-meaningful.friend.js'
+import friendSupplement from '../deep-meaningful.friend.supplement.js'
 import relationshipPart1 from '../deep-meaningful.relationship.part1.js'
 import relationshipPart2 from '../deep-meaningful.relationship.part2.js'
 
 export default {
-  friend: publishBalanced(friend),
+  friend: publishBalanced(mergeGroups(friend, friendSupplement)),
   relationship: publishBalanced(mergeGroups(relationshipPart1, relationshipPart2)),
 }
