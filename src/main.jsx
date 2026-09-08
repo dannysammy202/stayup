c63a7776655e436ca5df4faaa6f9d7de782f88ab
dannysammy202 <98893120+dannysammy202@users.noreply.github.com>
2026-09-08 15:21:20 +0100

    Load static starter format styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppV2 from './AppV2.jsx'
+import './conversation-source.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
